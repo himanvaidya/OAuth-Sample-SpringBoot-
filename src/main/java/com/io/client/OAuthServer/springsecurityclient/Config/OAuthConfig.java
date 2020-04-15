1caefac8b1e4fb4674b3485464172b5599ee679b
himanvaidya <30551423+himanvaidya@users.noreply.github.com>
2020-04-15 23:29:33 +0530

    spring security client related changes added

---
+++ src/main/java/com/io/client/OAuthServer/springsecurityclient/Config/OAuthConfig.java
@@ -0,0 +1,22 @@
+package com.io.client.OAuthServer.springsecurityclient.Config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+@EnableOAuth2Sso
+@Configuration
+public class OAuthConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/logout")
+                .permitAll()
+                .anyRequest().authenticated();
+
+    }
+}
